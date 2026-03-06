05124d54286d9087762d28a0e1fd22f5db6bd26b
vickbyan10 <aigbologamiraclep@gmail.com>
2026-03-07 02:00:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const billInput = document.getElementById("billAmount");
+const tipButton = document.querySelectorAll(`.tip-btn`);
+const tipAmount = document.getElementById("tipAmount");
+const totalbill = document.getElementById("totalBill");
+let selectedTip = 0;
+tipButtons.forEach(function(button) {
+    button.addEventListener('click' , function(){
+        tipButton.forEach(function(btn){
+            btn.classList.remove('active')
+        });
+        button.classList.add('active');
+        selectedTip = button.dataset.tip;
+        calculateTip();
+    })
+})
+function calculateTip(){
+    const bill = parseFloat(billInput.value);
+    if(isNaN(bill)  || bill < o) return;
+    const tip = (bill * selectedTip) / 100;
+    const total = bill + tip;
+    tipAmount.textContent = '$' + tip.toFixed(2);
+    totalbill.textContent = '$' + total.toFixed(2);
+}
+billInput.addEventListener('input' , calculateTip);
+ 
